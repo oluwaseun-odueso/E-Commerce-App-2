db521969eda43c46a73a95f39fa0348f0ad6ed15
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2022-10-01 01:53:57 +0100

    updated config, added sequelizerc file

--- config/config.js
+++ config/config.js
@@ -1,4 +1,6 @@
-{
+require('dotenv').config()
+
+module.export = {
   "development": {
     "username": "root",
     "password": "animalHumanBeingCaramel100#",
@@ -14,10 +16,10 @@
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": "animalHumanBeingCaramel100#",
-    "database": "e-commerce",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.PROD_USERNAME,
+    "password": process.env.PROD_PASSWORD,
+    "database": process.env.PROD_DATABASE,
+    "host": process.env.PROD_HOSTNAME,
+    "dialect": "postgres"
   }
 }
